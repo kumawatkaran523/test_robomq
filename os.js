5e3e6a2940a8032f6a2218744c3dc9f48873c6d9
Karan Kumawat <kumawatkaran523@gmail.com>
2026-01-08 18:28:46 +0530

    json

--- os.js
+++ os.js
@@ -5,4 +5,13 @@ console.log(os.arch()); // system architecture
 console.log(os.totalmem()); // total RAM
 console.log(os.freemem()); // free RAM
 console.log(os.hostname()); // computer name
-console.log(os.cpus());
+
+const obj = {
+  name: "karan",
+  age: 21,
+};
+
+const jsonString = JSON.stringify(obj);
+console.log(jsonString);
+const jsonToObj = JSON.parse(jsonString);
+console.log(jsonToObj);
